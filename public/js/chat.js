b59b6d53aff9566cbcb195a9d3689e5262ba328f
Anupam Kumar <anupam@vkreate.in>
2020-05-20 23:10:22 +0530

    Init commit

---
+++ public/js/chat.js
@@ -0,0 +1,111 @@
+//Retrieving the query string
+const { username, room } = Qs.parse(location.search,{ignoreQueryPrefix:true})
+if(!username || !room){
+    window.location.href = '/'
+}
+
+const socket = io()
+
+const $errorSuccessMessage = document.querySelector('#errorSuccessMessage')
+const $messageForm         = document.querySelector('#chatForm')
+const $messageInput        = document.querySelector('#message')
+const $messageSubmit       = document.querySelector('#submitButton')
+const $chatArea            = document.querySelector('#chat-area')
+const $sendLocationButton  = document.querySelector('#send-location')
+const $sidebar             = document.querySelector('#sidebar')
+
+//Templates
+const $templateMessage     = document.querySelector('#template-message').innerHTML
+const $templateLocation    = document.querySelector('#template-location').innerHTML
+const $templateSidebar     = document.querySelector('#template-sidebar').innerHTML
+
+
+//Auto scroll
+
+const scrollToTop = ()=>{
+    $chatArea.scrollTop = $chatArea.scrollHeight
+}
+
+$messageForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    $messageSubmit.setAttribute('disabled','disabled')
+    $errorSuccessMessage.innerHTML =''
+
+    if(message.value==''){
+        $errorSuccessMessage.innerHTML ='<span class="text-danger">Please type in message box</span>'
+        $messageSubmit.removeAttribute('disabled')
+        return false
+    }
+
+    socket.emit('message',$messageInput.value,(error)=>{
+        if(error){
+            return console.log(error)
+        }
+
+        $messageSubmit.removeAttribute('disabled')
+        $messageInput.value=''
+        $messageInput.focus()
+        console.log('Message delivered!')
+    })
+
+})
+
+$sendLocationButton.addEventListener('click',()=>{
+    $errorSuccessMessage.innerHTML =''
+    if(!navigator.geolocation)
+    {
+        $errorSuccessMessage.innerHTML ='<span class="text-danger">Your browser does not support location sharing</span>'
+    }
+
+    $sendLocationButton.setAttribute('disabled','disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared!')  
+        })
+    })
+
+})
+
+socket.on('sendMessage',(message)=>{
+    const html = Mustache.render($templateMessage,
+        {
+            message:message.text,
+            createdAt:moment(message.createdAt).format('h:mm A'),
+            username:message.username
+        })
+    $chatArea.insertAdjacentHTML('beforeend',html)
+    scrollToTop()
+})
+
+socket.on('locationMessage',(location)=>{
+    const html = Mustache.render($templateLocation,
+        {
+            location:location.url,
+            createdAt:moment(location.createdAt).format('h:mm A'),
+            username:location.username
+        })
+    $chatArea.insertAdjacentHTML('beforeend',html)
+    scrollToTop()
+})
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
+})
+
+socket.on('roomData',({room,users})=>{
+    const html = Mustache.render($templateSidebar,{
+        room,
+        users
+    })
+
+    $sidebar.innerHTML= html
+})
+
---
+++ src/index.js
@@ -0,0 +1,81 @@
+const express   = require('express')
+const path      = require('path')
+const http      = require('http')
+const socketio  = require('socket.io')
+const Filter    = require('bad-words')
+const {generateMessage,generateLocationMessage} = require('./utils/messages')
+const {addUser,removeUser,getUser,getUsersInRoom} = require('./utils/users')
+
+//Set http instead express
+const app     = express()
+const server  = http.createServer(app)
+const io      = socketio(server)
+
+//Set Port
+const port    = process.env.port
+
+//Public Path
+const pathToPublic = path.join(__dirname,'../public')
+
+//Setup static directory to serve
+app.use(express.static(pathToPublic))
+
+io.on('connection',(socket)=>{
+    console.log('New connection on web socket!')
+
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
+
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
+
+        socket.emit('sendMessage', generateMessage('Admin','Welcome!'))
+        socket.broadcast.to(user.room).emit('sendMessage', generateMessage('Admin',`${user.username} has joined!`))
+        io.to(user.room).emit('roomData',{
+            room:user.room,
+            users:getUsersInRoom(user.room)
+        })
+        
+        callback()
+    })
+
+    socket.on('message',(message, callback)=>{
+
+        const user  = getUser(socket.id)
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Bad words not allowed')
+        }
+
+        io.to(user.room).emit('sendMessage',generateMessage(user.username, message))
+        callback()
+    })
+
+    socket.on('sendLocation',({latitude,longitude},callback)=>{
+        const user  = getUser(socket.id)
+        io.to(user.room).emit('locationMessage',generateLocationMessage(user.username, `https://google.com/maps?q=${latitude},${longitude}`))
+        callback('Location Shared!')
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('sendMessage', generateMessage('Admin',`${user.username} has left!`))
+            io.to(user.room).emit('roomData',{
+                room:user.room,
+                users:getUsersInRoom(user.room)
+            })
+        }
+    })
+
+})
+
+//Serve as http server express running behind it
+server.listen(port,()=>{
+    console.log(`Server is up and running on port:${port}`)
+})
